fb774b85b5e5722696fd528539be70595c2ae4f5
Mauri <linkinmjs@gmail.com>
2020-03-23 20:43:44 -0300

    Preguntando al usuario su presupuesto

---
+++ src/components/Pregunta.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+
+const Pregunta = () => {
+    return ( 
+        <Fragment>
+            <h2>Colocá tu presupuesto</h2>
+
+            <form>
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Colocá aquí tu presupuesto"
+                />
+                <input
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Definir presupuesto"
+                />                
+            </form>
+        </Fragment>
+    );
+}
+ 
+export default Pregunta;
