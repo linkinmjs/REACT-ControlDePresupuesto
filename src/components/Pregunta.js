b5ecd6c6cb0ad5e2d600524a6ee4b0b52ca191ad
Mauri <linkinmjs@gmail.com>
2020-03-24 20:00:56 -0300

    Validar un presupuesto

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -4,21 +4,28 @@ const Pregunta = () => {
 
     // definir el state
     const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
 
     // Submit para definir el presupuesto
     const agregarPresupuesto = e => {
         e.preventDefault();
 
         // validar
+        if(cantidad < 1 || isNaN(cantidad)){
+            guardarError(true);
+            return;
+        }
 
         // si pasa la validación
+        guardarError(false);
 
-        
     }
 
     return ( 
         <Fragment>
             <h2>Colocá tu presupuesto</h2>
+
+            {error? : null}
 
             <form
                 onSubmit = {agregarPresupuesto}
