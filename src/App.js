82caddbf27642560426a00bf5852b1adc205a09c
Mauri <linkinmjs@gmail.com>
2020-03-24 20:28:48 -0300

    Cargando un componente u otro

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import React, {useState} from 'react';
 import Pregunta from "./components/Pregunta";
 import Formulario from "./components/Formulario";
+import { act } from 'react-dom/test-utils';
 
 function App() {
 
   const [ presupuesto, guardarPresupuesto] = useState(0);
   const [ restante, guardarRestante] = useState(0);
+  const [ mostrarpregunta, actualizarPregunta ] = useState(true);
 
   return (
     <div className="container">
       <header>
         <h1>Gasto semanal</h1>
-        <div className="contenido-principal contenido">
-          <Pregunta 
-            guardarPresupuesto={guardarPresupuesto}
-            guardarRestante={guardarRestante}
-          />
 
-          <div className="row">
-            <div className="one-half column">
-              <Formulario />
+        <div className="contenido-principal contenido">
+          { mostrarpregunta ? (
+            <Pregunta 
+              guardarPresupuesto={guardarPresupuesto}
+              guardarRestante={guardarRestante}
+              actualizarPregunta={actualizarPregunta}
+            />
+          ) : (
+            <div className="row">
+              <div className="one-half column">
+                <Formulario />
+              </div>
+              <div className="one-half column">
+                2
+              </div>
             </div>
-            <div className="one-half column">
-              2
-            </div>
-          </div>
+          ) }
         </div>
       </header>
     </div>
